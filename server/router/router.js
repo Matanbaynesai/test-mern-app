0fcf310db15381708073ea118536c362eedcc611
matan <you@example.com>
2021-08-10 10:09:31 +0300

    changes

--- server/router/router.js
+++ server/router/router.js
@@ -2,8 +2,8 @@ const express = require("express");
 const myRouter = express.Router();
 const model = require("../controllers/controller");
 
-myRouter.get("/", model.getAllUsers);
-myRouter.post("/", model.createDocument);
+// myRouter.get("/", model.getAllUsers);
+// myRouter.post("/", model.createDocument);
 // myRouter.put("/", model.updateDocument);
 // myRouter.delete("/", model.deleteDocument);
 
